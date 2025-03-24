fb14cf30d2b2a7a677293a97c3bc9352adc0a10d
Anokatabc <julian.voigt1@gmail.com>
2025-03-24 15:24:40 +0100

    Rudimentärer TreeView funktioniert JavaFX auf Version 23 aktualisiert.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.example.musikplayer_doit {
 
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
+    requires javafx.media;
 
     opens org.example.musikplayer_doit to javafx.fxml;
     exports org.example.musikplayer_doit;
--- src/main/java/org/example/musikplayer_doit/controller/Controller.java
+++ src/main/java/org/example/musikplayer_doit/controller/Controller.java
@@ -1,16 +1,65 @@
 package org.example.musikplayer_doit.controller;
 
-//unused import javafx.fxml.FXML;
-//unused import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+
+
 
 public class Controller {
 
-    public void clickPlay () {
-        System.out.println("Playingssss");
+@FXML
+private TreeView<File> folderTreeView;
+
+    public void clickPlay() {
+        String uriString = new File("D:\\Musikmainaug2019\\Aku no Hana\\\uD83D\uDC40 Zankyou no Hana.mp3").toURI().toString();
+        MediaPlayer player = new MediaPlayer(new Media(uriString));
+        player.play();
+        System.out.println("Playing: " + uriString);
 
     }
 
-    public void emptyTest () {
-        System.out.println("Empty");
+//    @FXML
+//    public void initialize() {
+//        TreeItem<File> rootItem = new TreeItem<>(new File ("My Computer"));
+//        //rootItem.setExpanded(true);
+//        folderTreeView.setRoot(rootItem);
+//        File[] rootDrives = File.listRoots();
+//        if (rootDrives != null) {
+//            for (var rootDrive : rootDrives) {
+//                TreeItem<File> driveItem = new TreeItem<>(rootDrive);
+//                rootItem.getChildren().add(driveItem);
+//                createTree(rootDrive, driveItem);
+//            }
+//        }
+//    }
+
+@FXML
+public void initialize() {
+    File rootFile = new File("D:\\");
+    TreeItem<File> rootItem = new TreeItem<>(rootFile);
+    //rootItem.setExpanded(true);
+    folderTreeView.setRoot(rootItem);
+    createTree(rootFile, rootItem);
+}
+
+    //alternativ: populateTree
+    private void createTree(File file, TreeItem<File> parentItem) {
+        File[] files = file.listFiles();
+        if (files != null) {
+            for (var f : files) {
+                TreeItem<File> item = new TreeItem<>(f);
+                parentItem.getChildren().add(item);
+                if (f.isDirectory()) {
+                    createTree(f, item);
+                }
+            }
+        }
     }
+
+
 }
