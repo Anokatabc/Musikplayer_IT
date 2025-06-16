288c2d101d6590c960e68636022055618463c257
Anokata <julian.voigt1@gmail.com>
2025-06-16 19:11:01 +0200

    Eingliederung der SortBackslashes-Methode, die aus irgendeinem Grund fehlte.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module org.example.musikplayer_doit {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+    requires jaudiotagger;
+    requires java.desktop;
+    requires java.sql;
+
+
+    opens org.example.musikplayer_doit to javafx.fxml;
+    exports org.example.musikplayer_doit;
+    exports org.example.musikplayer_doit.controller;
+    exports org.example.musikplayer_doit.model;
+    opens org.example.musikplayer_doit.controller to javafx.fxml;
+    opens org.example.musikplayer_doit.model to javafx.base, javafx.fxml;
+
+}
